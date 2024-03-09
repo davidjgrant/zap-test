ed3986f81768922a49ce6ed4637f4476057ef8c6
David <davidgrant132@gmail.com>
2024-03-09 19:01:35 +0000

    Add styled-components compiler option and update padding in StyledPageLayout

--- src/components/Layouts/StyledPageLayout.ts
+++ src/components/Layouts/StyledPageLayout.ts
@@ -6,6 +6,10 @@ export const StyledPage = styled.main`
   align-items: center;
   justify-content: center;
   margin: 0 auto;
-  padding: 24px 32px;
+  padding: 24px 16px;
   max-width: 1440px;
+
+  @media screen and (min-width: 768px) {
+    padding: 24px 32px;
+  }
 `;
